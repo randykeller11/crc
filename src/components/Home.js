e89761f24e42f2f941302c6838b499bffdde06f4
Randy <randykeller1193@gmail.com>
2021-05-08 10:12:42 -0400

    fixed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,6 @@ function Home() {
       <h1>
         Welcome to Community Record Club a record club for the 21st century!
       </h1>
-      <h2>Login</h2>
       <button onClick={login}>Login</button>
     </div>
   );
