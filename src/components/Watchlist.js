f58e5b87dfd96a866def3b106875ead114548b38
Randy <randykeller1193@gmail.com>
2021-05-25 12:30:38 -0400

    initial commit

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 import db from "../config/firebase";
 import { useAuth } from "../auth-context";
+import { useFetch } from "../hooks/useFetch";
 
 function Watchlist() {
-  const { currentUser } = useAuth();
-  const [userData, setUserData] = useState(null);
+  const albums = [
+    {
+      index: 0,
+      id: 183133172,
+    },
+    {
+      index: 1,
+      id: 128447202,
+    },
+    {
+      index: 2,
+      id: 212686712,
+    },
+    {
+      index: 3,
+      id: 8835801,
+    },
+    {
+      index: 4,
+      id: 8114238,
+    },
+  ];
 
-  useEffect(() => {
-    if (currentUser) {
-      const observer = db.collection("users").doc(`${currentUser.uid}`);
-      observer.onSnapshot((snapshot) => {
-        console.log(snapshot.data());
-        setUserData(snapshot.data());
-      });
-    }
-  }, [currentUser]);
+  const { currentUser } = useAuth();
+  const [watchlistAlbums, setWatchlistAlbums] = useState([]);
+  const url = "http://localhost:4000/album/183133172/";
+  const [result, error, isLoading] = useFetch(url);
 
   return (
     <div>
@@ -24,3 +40,17 @@ function Watchlist() {
 }
 
 export default Watchlist;
+
+//code for connecting database
+
+//   const [userData, setUserData] = useState(null);
+
+//   useEffect(() => {
+//     if (currentUser) {
+//       const observer = db.collection("users").doc(`${currentUser.uid}`);
+//       observer.onSnapshot((snapshot) => {
+//         console.log(snapshot.data());
+//         setUserData(snapshot.data());
+//       });
+//     }
+//   }, [currentUser]);
