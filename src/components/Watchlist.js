88c451e023bea7a8ea737e529716cba39579e7fe
Randy <randykeller1193@gmail.com>
2021-05-27 10:38:52 -0400

    key changes

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,51 +1,45 @@
 import React, { useState, useEffect } from "react";
 import db from "../config/firebase";
 import { useAuth } from "../auth-context";
-import { useFetch } from "../hooks/useFetch";
+import SearchTest from "SearchTest";
 
 function Watchlist() {
-  const albums = [
-    {
-      index: 0,
-      id: 183133172,
-    },
-    {
-      index: 1,
-      id: 128447202,
-    },
-    {
-      index: 2,
-      id: 212686712,
-    },
-    {
-      index: 3,
-      id: 8835801,
-    },
-    {
-      index: 4,
-      id: 8114238,
-    },
-  ];
+  //   const albums = [
+  //     {
+  //       index: 0,
+  //       id: 183133172,
+  //     },
+  //     {
+  //       index: 1,
+  //       id: 128447202,
+  //     },
+  //     {
+  //       index: 2,
+  //       id: 212686712,
+  //     },
+  //     {
+  //       index: 3,
+  //       id: 8835801,
+  //     },
+  //     {
+  //       index: 4,
+  //       id: 8114238,
+  //     },
+  //   ];
 
   const { currentUser } = useAuth();
   const [watchlistAlbums, setWatchlistAlbums] = useState([]);
-  const url = "http://localhost:4000/album/184480112/";
-  const [result, error, isLoading] = useFetch(url);
-
-  useEffect(() => {
-    if (result) {
-      console.log(result.cover);
-    }
-  }, [result]);
 
   return (
     <div className="watchlist">
-      {result && (
+      <h1>welcome to the watchlist</h1>
+      <SearchTest />
+      {/* {result && (
         <div className="watchlist__card">
           <h1>{result.title}</h1>
           <img src={`${result.cover_big}`} />
         </div>
-      )}
+      )} */}
     </div>
   );
 }
