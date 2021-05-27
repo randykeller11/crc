7acf49c9580ab8df15d42ed66d1a660e710960be
Randy <randykeller1193@gmail.com>
2021-05-27 11:49:48 -0400

    watchlist rendering

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -4,42 +4,77 @@ import { useAuth } from "../auth-context";
 import SearchTest from "./SearchTest";
 
 function Watchlist() {
-  //   const albums = [
-  //     {
-  //       index: 0,
-  //       id: 183133172,
-  //     },
-  //     {
-  //       index: 1,
-  //       id: 128447202,
-  //     },
-  //     {
-  //       index: 2,
-  //       id: 212686712,
-  //     },
-  //     {
-  //       index: 3,
-  //       id: 8835801,
-  //     },
-  //     {
-  //       index: 4,
-  //       id: 8114238,
-  //     },
-  //   ];
+  const albums = [
+    {
+      index: 0,
+      id: 183133172,
+    },
+    {
+      index: 1,
+      id: 128447202,
+    },
+    {
+      index: 2,
+      id: 212686712,
+    },
+    {
+      index: 3,
+      id: 8835801,
+    },
+    {
+      index: 4,
+      id: 8114238,
+    },
+  ];
 
   const { currentUser } = useAuth();
-  const [watchlistAlbums, setWatchlistAlbums] = useState([]);
+  const [watchlistAlbums, setWatchlistAlbums] = useState(albums);
+  const [watchlistData, setWatchListData] = useState([]);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loadStatus, setLoadStatus] = useState(0);
+
+  useEffect(() => {
+    const retrievalFunction = async (album) => {
+      try {
+        const response = await fetch(`http://localhost:4000/album/${album.id}`);
+        const data = await response.json();
+        const albumObject = { id: album.id, albumData: data };
+        setResult(albumObject);
+      } catch (error) {
+        console.error("error from fetch: ", error);
+        setError(error.message);
+      }
+    };
+    const getData = () => {
+      setLoadStatus(1);
+      watchlistAlbums.forEach((album) => {
+        console.log(album);
+        retrievalFunction(album);
+      });
+    };
+    getData();
+  }, []);
+
+  useEffect(() => {
+    console.log(result, "🏆🍩");
+    if (result) {
+      const localArray = [...watchlistData];
+      localArray.push(result);
+      setWatchListData(localArray);
+    }
+  }, [result]);
 
   return (
     <div className="watchlist">
       <h1>welcome to the watchlist</h1>
-      <SearchTest />
-      {/* {result && (
-        <div className="watchlist__card">
-          <h1>{result.title}</h1>
-          <img src={`${result.cover_big}`} />
-        </div>
-      )} */}
+      {watchlistData.length === watchlistAlbums.length &&
+        watchlistData.map((_album) => (
+          <div>
+            <h1>{_album.albumData.title}</h1>
+            <img src={`${_album.albumData.cover_big}`} />
+          </div>
+        ))}
     </div>
   );
 }
