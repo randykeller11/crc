316047bbc1b6780c6af3b6f9b6880c49ba21f9ea
Randy <randykeller1193@gmail.com>
2021-07-27 11:27:07 -0400

    deleted unecessary comments in watchlist

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -4,16 +4,10 @@ import db from "../config/firebase";
 import AlbumSearch from "./AlbumSearch";
 
 function Watchlist() {
-  // const profileData = useProfileData();
   const userData = useFirestoreData("users");
   const [payload, setPayload] = useState(null);
   const [watchlist, setWatchlist] = useState([]);
   const [isSearching, setIsSearching] = useState(false);
-  // const [userTest, setUserTest] = useState();
-  // const [watchlistData, setWatchListData] = useState([]);
-  // const [result, setResult] = useState(null);
-  // const [error, setError] = useState(null);
-  // const [loadStatus, setLoadStatus] = useState(0);
 
   //write payload to db dynamically
   useEffect(() => {
@@ -53,51 +47,3 @@ function Watchlist() {
   );
 }
 export default Watchlist;
-
-//-----------------------function to retrieve albums and render----------------------
-// useEffect(() => {
-//   const retrievalFunction = async (album) => {
-//     try {
-//       const response = await fetch(`http://localhost:4000/album/${album.id}`);
-//       const data = await response.json();
-//       const albumObject = { id: album.id, albumData: data };
-//       setResult(albumObject);
-//     } catch (error) {
-//       console.error("error from fetch: ", error);
-//       setError(error.message);
-//     }
-//   };
-//   const getData = () => {
-//     setLoadStatus(1);
-//     watchlistAlbums.forEach((album) => {
-//       console.log(album);
-//       retrievalFunction(album);
-//     });
-//   };
-//   if (watchlistAlbums) {
-//     getData();
-//   }
-// }, [watchlistAlbums]);
-
-// useEffect(() => {
-//   console.log(result, "🏆🍩");
-//   if (result) {
-//     const localArray = [...watchlistData];
-//     localArray.push(result);
-//     setWatchListData(localArray);
-//   }
-// }, [result]);
-
-//   return (
-//     <div className="watchlist">
-//       <h1>welcome to the watchlist</h1>
-//       {watchlistData.length === watchlistAlbums.length &&
-//         watchlistData.map((_album) => (
-//           <div>
-//             <h1>{_album.albumData.title}</h1>
-//             <img src={`${_album.albumData.cover_big}`} />
-//           </div>
-//         ))}
-//     </div>
-//   );
-// }
