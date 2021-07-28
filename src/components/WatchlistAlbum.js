0de8f7a920c5b17995ac017e46fe6013e4dae67b
Randy <randykeller1193@gmail.com>
2021-07-28 19:35:08 -0400

    made text smaller

--- src/components/WatchlistAlbum.js
+++ src/components/WatchlistAlbum.js
@@ -16,7 +16,7 @@ function WatchlistAlbum({ _album, _index, setSearchTarget }) {
   }
   return (
     <div className="album">
-      <h1>{_album.title}</h1>
+      <h3>{_album.title}</h3>
       <img src={`${_album.cover}`} alt="" />
     </div>
   );
