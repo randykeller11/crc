4799faf8f21bc8efb1442a980f3b16cc09248815
Randy <randykeller1193@gmail.com>
2021-07-27 23:14:25 -0400

    fixed bug in buildWatchlistDisplay function

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -11,7 +11,15 @@ function Watchlist() {
   const [payload, setPayload] = useState(null);
   const [dbWatchlist, setDbWatchlist] = useState(null);
   const [localWatchlist, setLocalWatchlist] = useState(null);
+  const [watchlistDisplay, setWatchlistDisplay] = useState([
+    { rowNum: 0, albums: [] },
+    { rowNum: 1, albums: [] },
+    { rowNum: 2, albums: [] },
+    { rowNum: 3, albums: [] },
+  ]);
+
   const [isSearching, setIsSearching] = useState(false);
+  const [editMode, setEditMode] = useState(0);
 
   useEffect(() => {
     if (userData) {
@@ -19,44 +27,39 @@ function Watchlist() {
     }
   }, [userData]);
 
-  //write payload to db dynamically
-  useEffect(() => {
-    if (payload) {
-      writeToDb("watchlists", payload, userData.id);
-      setPayload(null);
+  const buildWatchlistDisplay = (_array) => {
+    let localArray = [];
+    console.log(_array.length);
+    for (let i = 0; i < 20; i++) {
+      if (i < _array.length) {
+        localArray.push(_array[i]);
+      } else {
+        localArray.push(0);
+      }
     }
-  }, [payload]);
+    return localArray;
+  };
 
   useEffect(() => {
     if (dbWatchlist) {
       console.log(dbWatchlist.watchlist);
-      let localArray = [];
-      for (let i = 0; i < 20; i++) {
-        if (i < dbWatchlist.watchlist.length) {
-          localArray.push(dbWatchlist.watchlist[i]);
-        } else {
-          localArray.push(0);
-        }
-      }
-      setLocalWatchlist(localArray);
+      let testDisplay = buildWatchlistDisplay(dbWatchlist.watchlist);
+      setLocalWatchlist(testDisplay);
     }
   }, [dbWatchlist]);
 
-  const testButton = (
-    <button
-      onClick={() => {
-        setIsSearching(true);
-        setPayload({ emoji: "🍩", testPhrase: "donut" });
-      }}
-    >
-      edit
-    </button>
-  );
-
   //------------------primary watchlist component------------------
+  const handleClick = () => {
+    setIsSearching(true);
+    setEditMode(1);
+  };
+
   if (userData) {
     return (
       <div className="watchlist">
+        <button onClick={handleClick}>
+          {editMode === 1 ? "confirm" : "edit"}
+        </button>
         {!isSearching && (
           <div className="watchlist__console">
             {localWatchlist &&
@@ -84,3 +87,22 @@ function Watchlist() {
   return <h1>loading watchlist...</h1>;
 }
 export default Watchlist;
+
+//write payload to db dynamically
+// useEffect(() => {
+//   if (payload) {
+//     writeToDb("watchlists", payload, userData.id);
+//     setPayload(null);
+//   }
+// }, [payload]);
+
+// const testButton = (
+//   <button
+//     onClick={() => {
+//       setIsSearching(true);
+//       setPayload({ emoji: "🍩", testPhrase: "donut" });
+//     }}
+//   >
+//     edit
+//   </button>
+// );
