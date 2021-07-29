f59e9ea74a98ef5e72816912c61a80a194c49466
Randy <randykeller1193@gmail.com>
2021-07-28 20:56:47 -0400

    css adjustments

--- src/components/WatchlistAlbum.js
+++ src/components/WatchlistAlbum.js
@@ -5,17 +5,23 @@ function WatchlistAlbum({ _album, _index, setSearchTarget }) {
   if (_album === 0) {
     return (
       <div
-        className="album"
+        className="placeholderAlbum"
         onClick={() => {
           setSearchTarget(_index);
         }}
       >
-        <h1>no album loaded: {_index}</h1>
+        <img
+          src="https://www.northernsun.com/images/image16x16/576x576/0010.png"
+          alt=""
+        />
+
+        <h5>{_index}</h5>
       </div>
     );
   }
   return (
     <div className="album">
+      <h3 className="delete">x</h3>
       <img src={`${_album.cover}`} alt="" />
       <h3>{_album.title}</h3>
       <h5>{_album.artist}</h5>
