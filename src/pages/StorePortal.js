6af818c32062e164bfc63d13654589f95b4b93ab
Randy <randykeller1193@gmail.com>
2021-07-30 14:55:14 -0400

    added new searchbar component using new file structure

--- src/pages/StorePortal.js
+++ src/pages/StorePortal.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "../components/SearchBar";
 
 function StorePortal() {
   return (
     <div>
-      <h1>Store Portal connected</h1>
+      <SearchBar />
     </div>
   );
 }
