4ebf15fe3ab4f730c6f1da063b9fea41ebcdc859
Randy <randykeller1193@gmail.com>
2021-08-19 16:02:35 -0400

    set up third search format

--- src/components/AddAlbumToStore.js
+++ src/components/AddAlbumToStore.js
@@ -9,11 +9,17 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
   const [priceTarget, setPriceTarget] = useState(null);
   const [albumUPC, setAlbumUPC] = useState(null);
   const [isSearching, setIsSearching] = useState(true);
-  const [searchResult, setSearchResult] = useState(null);
+  const [searchResult, setSearchResult] = useState({
+    strAlbum: "",
+    strArtist: "",
+    intYearReleased: "",
+    strLabel: "",
+  });
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [lowPrice, setLowPrice] = useState(null);
   const [highPrice, setHighPrice] = useState(null);
-  const [searchCRC, setSearchCRC] = useState(false);
+  const [infoSource, setInfoSource] = useState("albumPages");
+  const [albumImage, setAlbumImage] = useState(null);
 
   useEffect(() => {
     if (isSubmitted) {
@@ -34,6 +40,10 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
 
       let cleanAlbumData = removeNullProperties(searchResult);
 
+      if (infoSource === "user") {
+        cleanAlbumData.strAlbumThumb = albumImage;
+      }
+
       let formattedPriceTarget = lowPrice
         ? { high: highPrice, low: lowPrice }
         : priceTarget;
@@ -44,29 +54,93 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
         formatTags: formatTags,
         priceTarget: formattedPriceTarget,
         albumUPC: albumUPC,
+        infoSource: infoSource,
       };
       setNewAlbumObject(objectToSubmit);
       setAddAlbumMode(false);
     }
   }, [isSubmitted]);
 
+  const componentBottom = (
+    <>
+      <div className="conditionOptions">
+        <label>Condition: </label>
+        <select
+          id="condition"
+          onChange={(e) => {
+            setCondition(e.target.value);
+          }}
+        >
+          <option value="M">M</option>
+          <option value="NM">NM</option>
+          <option value="VG+">VG+</option>
+          <option value="VG">VG</option>
+          <option value="G+">G+</option>
+        </select>
+      </div>
+      <input
+        type="text"
+        placeholder="FormatTags"
+        value={formatTags}
+        onChange={(e) => {
+          e.preventDefault();
+          setFormatTags(e.target.value);
+        }}
+      />
+      <div className="priceInput">
+        <input
+          type="number"
+          placeholder="$"
+          value={priceTarget}
+          onChange={(e) => {
+            e.preventDefault();
+            setPriceTarget(e.target.value);
+          }}
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="UPC"
+        value={albumUPC}
+        onChange={(e) => {
+          e.preventDefault();
+          setAlbumUPC(e.target.value);
+        }}
+      />
+      <button onClick={() => setIsSubmitted(true)}>Submit</button>
+    </>
+  );
+
   return (
     <div className="addAlbum">
       {isSearching && (
         <>
-          <h5
-            onClick={() => {
-              setSearchCRC(true);
-            }}
-          >
-            dont see your album?
-          </h5>
-          {searchCRC ? (
+          {infoSource === "albumPages" && (
+            <h5
+              onClick={() => {
+                setInfoSource("audioDB");
+              }}
+            >
+              Don't see your album?
+            </h5>
+          )}
+          {infoSource === "audioDB" && (
+            <h5
+              onClick={() => {
+                setInfoSource("user");
+                setIsSearching(false);
+              }}
+            >
+              Still don't see your album?
+            </h5>
+          )}
+          {infoSource === "albumPages" && (
             <CRCInvSearch
               setIsSearching={setIsSearching}
               setResult={setSearchResult}
             />
-          ) : (
+          )}
+          {infoSource === "audioDB" && (
             <SearchBar
               setIsSearching={setIsSearching}
               setResult={setSearchResult}
@@ -74,7 +148,7 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
           )}
         </>
       )}
-      {!isSearching && searchResult && (
+      {!isSearching && searchResult && infoSource != "user" && (
         <div className="albumCard">
           <img src={searchResult.strAlbumThumb} alt="" />
           <div className="albumInfo">
@@ -82,51 +156,76 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
             <h3>{searchResult.strArtist}</h3>
             <h4>{searchResult.intYearReleased}</h4>
             <h3>{searchResult.strLabel}</h3>
-            <div className="conditionOptions">
-              <label>Condition: </label>
-              <select
-                id="condition"
-                onChange={(e) => {
-                  setCondition(e.target.value);
-                }}
-              >
-                <option value="M">M</option>
-                <option value="NM">NM</option>
-                <option value="VG+">VG+</option>
-                <option value="VG">VG</option>
-                <option value="G+">G+</option>
-              </select>
-            </div>
+            {componentBottom}
+          </div>
+        </div>
+      )}
+      {!isSearching && infoSource === "user" && (
+        <div className="albumCard">
+          {!albumImage ? (
+            <button
+              onClick={() => {
+                setAlbumImage(searchResult.strAlbumThumb);
+              }}
+            >
+              add image
+            </button>
+          ) : (
+            <img src={albumImage} />
+          )}
+          <div className="albumInfo">
             <input
               type="text"
-              placeholder="FormatTags"
-              value={formatTags}
+              placeholder="album image"
+              value={albumImage}
               onChange={(e) => {
                 e.preventDefault();
-                setFormatTags(e.target.value);
+                setSearchResult({
+                  ...searchResult,
+                  strAlbumThumb: e.target.value,
+                });
               }}
             />
-            <div className="priceInput">
-              <input
-                type="number"
-                placeholder="$"
-                value={priceTarget}
-                onChange={(e) => {
-                  e.preventDefault();
-                  setPriceTarget(e.target.value);
-                }}
-              />
-            </div>
             <input
               type="text"
-              placeholder="UPC"
-              value={albumUPC}
+              placeholder="album title"
+              value={searchResult.strAlbum}
               onChange={(e) => {
                 e.preventDefault();
-                setAlbumUPC(e.target.value);
+                setSearchResult({ ...searchResult, strAlbum: e.target.value });
               }}
             />
-            <button onClick={() => setIsSubmitted(true)}>Submit</button>
+            <input
+              type="text"
+              placeholder="artist"
+              value={searchResult.strArtist}
+              onChange={(e) => {
+                e.preventDefault();
+                setSearchResult({ ...searchResult, strArtist: e.target.value });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="release year"
+              value={searchResult.intYearReleased}
+              onChange={(e) => {
+                e.preventDefault();
+                setSearchResult({
+                  ...searchResult,
+                  intYearReleased: e.target.value,
+                });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="label"
+              value={searchResult.strLabel}
+              onChange={(e) => {
+                e.preventDefault();
+                setSearchResult({ ...searchResult, strLabel: e.target.value });
+              }}
+            />
+            {componentBottom}
           </div>
         </div>
       )}
