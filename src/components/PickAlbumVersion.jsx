ec221614a968b30e4f13018cbc1cf3bc0384d043
Randy <randykeller1193@gmail.com>
2021-08-25 12:14:49 -0400

    button to show all options

--- src/components/PickAlbumVersion.jsx
+++ src/components/PickAlbumVersion.jsx
@@ -4,13 +4,24 @@ import "./PickAlbumVersion.css";
 
 function PickAlbumVersion({ setResult, masterResult }) {
   const [masterVersions, setMasterVersions] = useState(null);
+  const [showAllOptions, setShowAllOptions] = useState(false);
 
   useEffect(() => {
     getMasterVersions(masterResult.value.master_url, setMasterVersions);
   }, []);
   return (
     <div className="pickVersion">
+      {!showAllOptions && (
+        <h3
+          onClick={() => {
+            setShowAllOptions(true);
+          }}
+        >
+          show all
+        </h3>
+      )}
       {masterVersions &&
+        !showAllOptions &&
         masterResult.searchType === 2 &&
         masterVersions.versions.map((version) => {
           if (
@@ -34,6 +45,22 @@ function PickAlbumVersion({ setResult, masterResult }) {
             );
           }
         })}
+      {showAllOptions &&
+        masterVersions.versions.map((version) => (
+          <div
+            className="version__option"
+            onClick={() => {
+              setResult(version.resource_url);
+            }}
+          >
+            <h3>{version.title}</h3>
+            <h3>{version.label}</h3>
+            <h3>{version.released}</h3>
+            <h3>Format: {version.format}</h3>
+            <h3>Cat#:{version.catno}</h3>
+            <h3>Country:{version.country}</h3>
+          </div>
+        ))}
     </div>
   );
 }
