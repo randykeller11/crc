d8676998ef5d3d0693921e7b7f772f8593d4b860
Randy <randykeller1193@gmail.com>
2021-09-01 22:22:05 -0400

    handles new backend design with only discogs for search

--- src/components/AddAlbumToStore.js
+++ src/components/AddAlbumToStore.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
-import CRCInvSearch from "./CRCInvSearch";
 import DiscogsSearch from "./DiscogsSearch";
 import "./AddAlbumToStore.css";
 import PickAlbumVersion from "./PickAlbumVersion";
@@ -14,27 +12,17 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
   const [masterResult, setMasterResult] = useState(null);
   const [searchResult, setSearchResult] = useState(null);
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [infoSource, setInfoSource] = useState("albumPages");
   const [albumImage, setAlbumImage] = useState(null);
   const [releaseURL, setReleaseURL] = useState(null);
 
   useEffect(() => {
     if (isSubmitted) {
-      if (infoSource === "albumPages") {
-        setAlbumImage(searchResult.image);
-        setSearchResult({
-          ...searchResult,
-          docID: searchResult.docID,
-        });
-      }
-
       const objectToSubmit = {
         masterImage: albumImage,
         albumData: searchResult,
         sleeveCondition: sleeveCondition,
         mediaCondition: mediaCondition,
         priceTarget: priceTarget,
-        infoSource: infoSource,
       };
       setNewAlbumObject(objectToSubmit);
       setAddAlbumMode(false);
@@ -98,41 +86,16 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
     <div className="addAlbum">
       {isSearching && (
         <>
-          {infoSource === "albumPages" && (
-            <h5
-              onClick={() => {
-                setInfoSource("audioDB");
-              }}
-            >
-              Don't see your album?
-            </h5>
-          )}
-          {infoSource === "albumPages" && (
-            <CRCInvSearch
-              setIsSearching={setIsSearching}
-              setResult={setSearchResult}
-            />
-          )}
-          {infoSource === "audioDB" && (
+          {
             <DiscogsSearch
               setIsSearching={setIsSearching}
               setResult={setMasterResult}
             />
-          )}
+          }
         </>
       )}
-      {!isSearching && searchResult && infoSource === "albumPages" && (
-        <div className="albumCard">
-          <img src={searchResult.cover_image} alt="" />
-          <div className="albumInfo">
-            <h3>{searchResult.title}</h3>
-            <h4>{searchResult.albumInfo.intYearReleased}</h4>
-            <h3>{searchResult.albumInfo.strLabel}</h3>
-            {componentBottom}
-          </div>
-        </div>
-      )}
-      {!isSearching && masterResult && infoSource === "audioDB" && (
+
+      {!isSearching && masterResult && (
         <div className="albumCard">
           <img src={masterResult.value.cover_image} alt="" />
           <div className="albumInfo">
@@ -146,75 +109,6 @@ function AddAlbumToStore({ setAddAlbumMode, setNewAlbumObject }) {
                 masterResult={masterResult}
               />
             )}
-          </div>
-        </div>
-      )}
-      {!isSearching && infoSource === "user" && (
-        <div className="albumCard">
-          {!albumImage ? (
-            <button
-              onClick={() => {
-                setAlbumImage(searchResult.strAlbumThumb);
-              }}
-            >
-              add image
-            </button>
-          ) : (
-            <img src={albumImage} />
-          )}
-          <div className="albumInfo">
-            <input
-              type="text"
-              placeholder="album image"
-              value={albumImage}
-              onChange={(e) => {
-                e.preventDefault();
-                setSearchResult({
-                  ...searchResult,
-                  strAlbumThumb: e.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="album title"
-              value={searchResult.strAlbum}
-              onChange={(e) => {
-                e.preventDefault();
-                setSearchResult({ ...searchResult, strAlbum: e.target.value });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="artist"
-              value={searchResult.strArtist}
-              onChange={(e) => {
-                e.preventDefault();
-                setSearchResult({ ...searchResult, strArtist: e.target.value });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="release year"
-              value={searchResult.intYearReleased}
-              onChange={(e) => {
-                e.preventDefault();
-                setSearchResult({
-                  ...searchResult,
-                  intYearReleased: e.target.value,
-                });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="label"
-              value={searchResult.strLabel}
-              onChange={(e) => {
-                e.preventDefault();
-                setSearchResult({ ...searchResult, strLabel: e.target.value });
-              }}
-            />
-            {componentBottom}
           </div>
         </div>
       )}
